faabe59e900c79c388b94d8c0b2f89989e9a8ad8
kaizel <risenalpha@gmail.com>
2024-06-12 15:47:06 -0600

    start bento

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#171717",
+        border: "#2C2C2C",
+        primary: "#E63946",
+      },
+    },
+  },
+  plugins: [],
+};
