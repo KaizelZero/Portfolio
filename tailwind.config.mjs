d7699ab71add73dafe668291fd061ab640d89704
kaizel <risenalpha@gmail.com>
2024-06-12 21:55:19 -0600

    created projects route and added dm sans

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+        dm_sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         background: "#171717",
