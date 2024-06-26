136b061176c28c55f0f695b2952387e2f2b6f729
kaizel <risenalpha@gmail.com>
2024-06-26 11:31:31 -0600

    update blog style. needs post style

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,22 @@
+export function removeTrailingSlash(pathname: string) {
+  const matchTrailingSlash = /\w+\/$/;
+  if (matchTrailingSlash.test(pathname)) return pathname.slice(0, -1);
+  return pathname;
+}
+
 export function formatDate(date: Date): string {
-  return date.toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  // Format the date into 'MMMM DD, YYYY'
+  return new Intl.DateTimeFormat("en-US", {
+    month: "long", // Full month name
+    day: "2-digit", // Two-digit day
+    year: "numeric", // Four-digit year
+  }).format(date);
+}
+
+export function slugify(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-");
 }
