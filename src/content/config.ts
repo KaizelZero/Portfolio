3504f12b1309641a3c0ca7f3ac7a0d138ee077af
kaizel <risenalpha@gmail.com>
2024-06-26 10:50:15 -0600

    define blog schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,24 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      publishedAt: z.date(),
+      description: z.string(),
+      isPublish: z.boolean(),
+      isDraft: z.boolean().default(false),
+      image: z.string().url().optional(),
+      youtube: z.string().url().optional(),
+      ogImage: image()
+        .refine((img) => img.width >= 1200 && img.height >= 630, {
+          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        })
+        .or(z.string())
+        .optional(),
+    }),
+});
 
 export const collections = {
   blog: blogCollection,
