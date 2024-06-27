9ce706a182a1bddd5957e2d5411fcfd23b0d8c8e
kaizel <risenalpha@gmail.com>
2024-06-27 06:50:43 -0600

    start over

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,4 @@
-// https://astro.build/config
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import { siteConfig } from "./src/data/config";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap(),
-    robotsTxt(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
-  site: siteConfig.url,
-  markdown: {
-    syntaxHighlight: "shiki",
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
-    shikiConfig: {
-      theme: "dracula",
-      wrap: false,
-    },
-  },
-});
+export default defineConfig({});
