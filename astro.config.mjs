657e00453524d79ba6682b675093b1a5bf47e250
kaizel <risenalpha@gmail.com>
2024-06-29 00:40:35 -0600

    add resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import expressiveCode from 'astro-expressive-code'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
+import rehypeExternalLinks from 'rehype-external-links'
+import remarkUnwrapImages from 'remark-unwrap-images'
 import { expressiveCodeOptions } from './src/site-config'
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +17,26 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false
     }),
+    sitemap(),
+    mdx(),
     icon()
-  ]
+  ],
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['nofollow, noopener, noreferrer']
+        }
+      ]
+    ],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: ['']
+      }
+    }
+  },
+  prefetch: true
 })
